91bb0173fe7f86a933ac25e506cabf447e74fa3b
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2023-03-28 10:50:31 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,76 @@
+# My hello world program 😄
+
+class H:
+    def H(self):
+        return "H"
+
+class e:
+    def e(self):
+        return "e"
+
+class l:
+    def l(self):
+        return "l"
+
+class l1:
+    def l1(self):
+        return "l"
+
+class o:
+    def o(self):
+        return "o"
+
+class space:
+    def space(self):
+        return " "
+
+class W:
+    def W(self):
+        return "W"
+
+class o1:
+    def o1(self):
+        return "o"
+
+class r:
+    def r(self):
+        return "r"
+
+class l2:
+    def l2(self):
+        return "l"
+
+class d:
+    def d(self):
+        return "d"
+
+class exclamation:
+    def exclamation(self):
+        return "!"
+
+H = H.H(self=0)
+e = e.e(self=0)
+l = l.l(self=0)
+l = l1.l1(self=0)
+o = o.o(self=0)
+space = space.space(self=0)
+W = W.W(self=0)
+o = o1.o1(self=0)
+r = r.r(self=0)
+l = l2.l2(self=0)
+d = d.d(self=0)
+exclamaion = exclamation.exclamation(self=0)
+
+if H == "H":
+    if e == "e":
+        if l == "l":
+            if l == "l":
+                if o == "o":
+                    if space == " ":
+                        if W == "W":
+                            if o == "o":
+                                if r == "r":
+                                    if l == "l":
+                                        if d == "d":
+                                            if exclamaion == "!":
+                                                print(H + e + l + l + o + space + W + o + r + l + d + exclamaion)
